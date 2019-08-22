e160ecd857cf5930edd4ea4f5f4f7a28a2ada753
Brad Mortensen <b.mort86@gmail.com>
2019-08-22 16:31:10 -0600

    Adds employee validity tests

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  
+  describe 'validations' do
+    it 'is valid' do
+      employee = Employee.new(first_name: "Jeff", last_name: "Baker") 
+      
+      expect(employee.valid?).to be true
+      expect(employee.errors.full_messages).to be_empty
+    end  
+    it 'is invalid without first name' do
+      employee = Employee.new(first_name: nil, last_name: "Baker") 
+      
+      expect(employee.valid?).to be false
+      expect(employee.errors.full_messages).to include("First name can't be blank")
+    end  
+    it 'is invalid without last name' do
+      employee = Employee.new(first_name: "Jeff", last_name: nil) 
+      
+      expect(employee.valid?).to be false
+      expect(employee.errors.full_messages).to include("Last name can't be blank")
+    end  
+  end
 end
